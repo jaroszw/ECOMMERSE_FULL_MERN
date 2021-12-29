7aa5511ae54740e45d3de8bdfff00d0c4347d9eb
Wojciech Jarosz <jaroszw@gmail.com>
2021-12-29 19:14:36 +0100

    Login/Register Component

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,17 +1,21 @@
-import React, { useContext } from "react";
-import { GlobalState } from "../../../GlobalState";
-import ProductItem from "../utils/productItem/ProductItem";
+import React, { useContext } from 'react';
+import { GlobalState } from '../../../GlobalState';
+import ProductItem from '../utils/productItem/ProductItem';
+import Loading from '../utils/loading/Loading';
 
 const Products = () => {
   const state = useContext(GlobalState);
   const [products] = state.productsAPI.products;
 
   return (
-    <div className="products">
-      {products.map((product) => {
-        return <ProductItem key={product._id} product={product} />;
-      })}
-    </div>
+    <React.Fragment>
+      <div className="products">
+        {products.map((product) => {
+          return <ProductItem key={product._id} product={product} />;
+        })}
+      </div>
+      {products.length === 0 && <Loading />}
+    </React.Fragment>
   );
 };
 export default Products;
--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -1,34 +1,17 @@
-import React from "react";
-// import BtnRender from "./BtnRender";
-import { Link } from "react-router-dom";
+import React from 'react';
+import BtnRender from './BtnRender';
 
-function ProductItem({ product, isAdmin, deleteProduct, handleCheck }) {
+function ProductItem({ product }) {
   return (
     <div className="product_card">
-      {isAdmin && (
-        <input
-          type="checkbox"
-          checked={product.checked}
-          onChange={() => handleCheck(product._id)}
-        />
-      )}
       <img src={product.images.url} alt="" />
-
       <div className="product_box">
         <h2 title={product.title}>{product.title}</h2>
         <span>${product.price}</span>
         <p>{product.description}</p>
       </div>
-      <div className="row_btn">
-        <Link id="btn_buy" to="#!">
-          Buy
-        </Link>
-        <Link id="btn_view" to={`/detail/${product._id}`}>
-          View
-        </Link>
-      </div>
 
-      {/* <BtnRender product={product} deleteProduct={deleteProduct} /> */}
+      <BtnRender product={product} />
     </div>
   );
 }
